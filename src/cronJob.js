67782b95a2f705482cf5d912f52f7d5fa66aefb8
Julius Olajumoke <juliusolajumokecoder@gmail.com>
2025-03-03 16:17:35 +0100

    new changes made to the cronjob

--- src/cronJob.js
+++ src/cronJob.js
@@ -1,6 +1,6 @@
 const cron = require('node-cron');
 
-// Schedule a cron job to run every 2 hours
-cron.schedule('0 */1 * * *', () => {
+// Schedule a cron job to run every 45 minutes
+cron.schedule('*/45 * * * *', () => {
     console.log('Hello, World!');
 });
